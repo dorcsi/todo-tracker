3c6257a712c93fcba32e3c913871f13a2bb7fd58
Dorottya Erdosi <erdosi.dorottya@gmail.com>
2023-03-24 13:29:32 +0100

    ag-grid, mat-form-field imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { CellClassParams, CellClickedEvent, CellStyle, ColDef, GridReadyEvent } from 'ag-grid-community';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-project3';
+    title = 'todo-project';
+
+    private agGrid: any;
+
+    // Each Column Definition results in one Column.
+    public columnDefs: ColDef[] = [
+        { field: 'deadline', headerName: 'Deadline', cellStyle: this.getCellStyle() },
+        { field: 'task' },
+        { field: 'location' },
+        { field: 'blocker' },
+        { field: 'isDone' }
+    ];
+
+    // DefaultColDef sets props common to all Columns
+    public defaultColDef: ColDef = {
+        sortable: true,
+        filter: true,
+    };
+
+    rowData = [
+        { deadline: "Toyota", task: "Celica", location: 35000, blocker: '' },
+        { deadline: "Ford", task: "Mondeo", location: 32000 },
+        { deadline: "Porsche", task: "Boxster", location: 72000 }
+    ];
+
+    // Example load data from server
+    onGridReady(params: GridReadyEvent) {
+        this.agGrid = params.api;
+    }
+
+    // Example of consuming Grid Event
+    onCellClicked(e: CellClickedEvent): void {
+        console.log('cellClicked', e);
+    }
+
+    getCellStyle() {
+        return (params: CellClassParams): CellStyle => {
+            return { background: 'blue' }
+        }
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
+import { AgGridModule } from 'ag-grid-angular';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ declarations: [
+   AppComponent
+ ],
+ imports: [
+   BrowserModule,
+   HttpClientModule,
+   AgGridModule,
+   NoopAnimationsModule,
+   MatInputModule,
+   MatFormFieldModule
+ ],
+ providers: [],
+ bootstrap: [AppComponent]
 })
 export class AppModule { }
